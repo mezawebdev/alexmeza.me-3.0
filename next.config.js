c4d06e5d886ee3155a17e02824b1971b25d7aeb8
Alex Meza <mezawebdev@gmail.com>
2023-09-05 14:44:32 -0700

    add internationalization

--- next.config.js
+++ next.config.js
@@ -1,6 +1,10 @@
 module.exports = {
-    env: {
-      sup: 'my-value',
-    },
-    trailingSlash: true
-}
+  env: {
+    sup: 'my-value',
+  },
+  trailingSlash: true,
+  i18n: {
+    locales: ['en'],
+    defaultLocale: 'en',
+  },
+};
